06c5bbb720f02b6ff8523c2b64c6a3c999aad909
Jan Michael Tangonan <janmichael.tangonan.2022@student.lhschools.org>
2019-03-11 14:06:28 -0400

    Created first method

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,18 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Method 1:Hello World!
+        firstMethod("World!");
+
+        }
+
+    public static void firstMethod(String name){
+
+    System.out.println("Hello " + name);
+
+    }
+
+}
